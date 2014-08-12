e0c8342fdd33cba6fbc8fae4118a87f9ad9d85f3
William Tran <william@aligent.com.au>
2014-08-12 17:54:28 +0930

    Added option to specify static block for Empty Cart text & custom promotional text

--- app/code/community/Aligent/Minicart/Helper/Data.php
+++ app/code/community/Aligent/Minicart/Helper/Data.php
@@ -1,3 +1,25 @@
 <?php
 
-class Aligent_Minicart_Helper_Data extends Mage_Core_Helper_Abstract {}
+class Aligent_Minicart_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    const EMPTY_CART_BLOCK_CONFIG = 'aligent_frontend/mini_cart/no_item_block';
+    const PROMOTIONAL_MESSAGE_BLOCK_CONFIG = 'aligent_frontend/mini_cart/promotional_block	';
+
+    public function getEmptyCartMessage() {
+        $vBlockId = Mage::getStoreConfig(self::EMPTY_CART_BLOCK_CONFIG);
+        if ($vBlockId) {
+            return $this->getLayout()->createBlock('cms/block')->setBlockId($vBlockId)->toHtml();
+        }
+        // return default text
+        return "You have no items in your shopping cart.";
+    }
+
+    public function getPromotionalMessage() {
+        $vBlockId = Mage::getStoreConfig(self::PROMOTIONAL_MESSAGE_BLOCK_CONFIG);
+        if ($vBlockId) {
+            return $this->getLayout()->createBlock('cms/block')->setBlockId($vBlockId)->toHtml();
+        }
+        // return default text
+        return "";
+    }
+}
