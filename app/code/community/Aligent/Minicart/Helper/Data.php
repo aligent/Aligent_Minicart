b3f0995d2707551cb8ea59afa063fc844554b62f
William Tran <william@aligent.com.au>
2014-08-18 19:24:53 +0930

    Fixed config path & bug when getting static block html

--- app/code/community/Aligent/Minicart/Helper/Data.php
+++ app/code/community/Aligent/Minicart/Helper/Data.php
@@ -2,8 +2,8 @@
 
 class Aligent_Minicart_Helper_Data extends Mage_Core_Helper_Abstract {
 
-    const EMPTY_CART_BLOCK_CONFIG = 'aligent_frontend/mini_cart/no_item_block';
-    const PROMOTIONAL_MESSAGE_BLOCK_CONFIG = 'aligent_frontend/mini_cart/promotional_block	';
+    const EMPTY_CART_BLOCK_CONFIG = 'aligent_frontend/mini_cart/empty_cart_block';
+    const PROMOTIONAL_MESSAGE_BLOCK_CONFIG = 'aligent_frontend/mini_cart/promotional_block';
 
     public function getEmptyCartMessage() {
         $vBlockId = Mage::getStoreConfig(self::EMPTY_CART_BLOCK_CONFIG);
@@ -17,7 +17,7 @@ class Aligent_Minicart_Helper_Data extends Mage_Core_Helper_Abstract {
     public function getPromotionalMessage() {
         $vBlockId = Mage::getStoreConfig(self::PROMOTIONAL_MESSAGE_BLOCK_CONFIG);
         if ($vBlockId) {
-            return $this->getLayout()->createBlock('cms/block')->setBlockId($vBlockId)->toHtml();
+            return Mage::app()->getLayout()->createBlock('cms/block')->setBlockId($vBlockId)->toHtml();
         }
         // return default text
         return "";
